0867ce46bf2b26323bc35a695f33deaf04aa6ab3
Albert van Dalen <albertfora@avdweb.nl>
2019-10-07 15:54:05 +0200

    Delete Functions.h

--- Functions.h
+++
@@ -1,6 +0,0 @@
-#ifndef _Functions_H_
-#define _Functions_H_
-
-void drawColorTextLine(int line, int left, String str, uint16_t textColor=TFT_WHITE, uint16_t backgroundColor=TFT_BLACK); // line 0..8
-
-#endif
